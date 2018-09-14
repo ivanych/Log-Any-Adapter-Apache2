f2dc3a9f0c2ebcefc213012c3a29f6ec0a69f5b1
Mikhail Ivanov <m.ivanych@gmail.com>
2018-09-15 00:01:38 +0300

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Log::Any::Adapter::Apache2 - It's new $module
+
+# SYNOPSIS
+
+    use Log::Any::Adapter::Apache2;
+
+# DESCRIPTION
+
+Log::Any::Adapter::Apache2 is ...
+
+# LICENSE
+
+Copyright (C) Mikhail Ivanov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mikhail Ivanov <m.ivanych@gmail.com>
---
+++ lib/Log/Any/Adapter/Apache2.pm
@@ -0,0 +1,39 @@
+package Log::Any::Adapter::Apache2;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Log::Any::Adapter::Apache2 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Log::Any::Adapter::Apache2;
+
+=head1 DESCRIPTION
+
+Log::Any::Adapter::Apache2 is ...
+
+=head1 LICENSE
+
+Copyright (C) Mikhail Ivanov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mikhail Ivanov E<lt>m.ivanych@gmail.comE<gt>
+
+=cut
+
